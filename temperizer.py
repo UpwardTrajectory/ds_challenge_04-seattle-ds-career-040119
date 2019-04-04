b0f8b1a7b4d0037c23fff43e4ba780e4f59b4b52
“David <dakaspar@gmail.com>
2019-04-04 09:21:24 -0700

    add Kelvin and check for absolute zero

--- temperizer.py
+++ temperizer.py
@@ -9,4 +9,18 @@ def convert_f_to_c(temperature_f):
 
 def convert_c_to_f(temperature_c):
     """Convert Celsius to Fahrenheit."""
-    pass
+    return (temperature_c*1.8)+32
+
+def convert_c_to_k(temp_c):
+    temp_k = temp_c + 273.15
+    return temp_k if temp_k >= 0 else "Can't be colder than absolute zero."
+
+def convert_f_to_k(temp_f):
+    return convert_c_to_k(convert_f_to_c(temp_f))
+
+def convert_k_to_c(temp_k):
+    temp_c = temp_k - 273.15
+    return temp_c if temp_c >= -273.15 else "Can't be colder than absolute zero."
+
+def convert_k_to_f(temp_k):
+    return convert_k_to_c(convert_c_to_f(temp_k)) 
